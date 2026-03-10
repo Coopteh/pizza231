670215663cc0b35ffd83ca86199f4371d2d8155b
unknown <student@KCT-409-05.coopteh.local>
2026-03-10 16:37:46 +0700

    Шаблон базовой страницы

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+use App\Views\BaseTemplate;
+
+$template = BaseTemplate::getTemplate();
+$resultTemplate =  sprintf($template, "Основная страница", "Просто текст");
+echo $resultTemplate;

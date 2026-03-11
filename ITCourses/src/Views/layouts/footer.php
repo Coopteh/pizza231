4624a28983cc9cfb9628f99eb75f4cb23f2c2dc5
kasll14 <toni.maslennikov.08@inbox.ru>
2026-03-11 14:33:14 +0300

    123

--- ITCourses/src/Views/layouts/footer.php
+++
@@ -1,7 +0,0 @@
-    </main>
-    <footer>
-        <p>&copy; 2024 ITCourses. Все права защищены.</p>
-    </footer>
-    <script src="<?= BASE_URL ?>assets/js/main.js"></script>
-</body>
-</html>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require_once("./vendor/autoload.php");
+use App\Controllers\HomeController;
+
+$controller = new HomeController();
+echo $controller->get();

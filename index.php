c82f046ec96d708107ba242d75cb52d3a9501973
kasll14 <toni.maslennikov.08@inbox.ru>
2026-03-13 10:06:52 +0300

    123

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require_once("./vendor/autoload.php");
-use App\Controllers\HomeController;
+use App\Routes\Router;
 
-$controller = new HomeController();
-echo $controller->get();
+$url = $_SERVER['REQUEST_URI'];
+$controller = new Router();
+echo $controller->route($url);

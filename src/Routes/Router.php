e3434ab22c44e3a95f18caa580e9156e4da33068
Владислав Милевский <coopteh231@mail.ru>
2026-04-06 16:55:10 +0700

    Рефакторинг Router

--- src/Routes/Router.php
+++ src/Routes/Router.php
@@ -8,11 +8,69 @@ use App\Controllers\BasketController;
 use App\Controllers\OrderController;
 
 class Router {
+    private int $id = 0;
+
+    private function getRoutes(): array {
+        return [
+            'about' => ['controller' => AboutController::class, 
+                        'method' => 'get'],
+            'products' => ['controller' => ProductController::class, 
+                        'method' => 'get', 
+                        'params' => ['id' => $this->id]],
+            'basket' => ['controller' => BasketController::class, 
+                        'method' => 'add', 
+                        'redirect' => true],
+            'order' => [
+                'GET' => ['controller' => OrderController::class, 
+                        'method' => 'get'],
+                'POST' => ['controller' => OrderController::class, 
+                        'method' => 'create'],
+            ],
+            'basket_clear' => ['controller' => BasketController::class,
+                        'method' => 'clear',                         
+                        'redirect' => true],
+        ];
+    }
+
+    private function executeRoute(array $route, array $pieces): string {
+        $controller = new $route['controller']();
+        $params = ($this->id) ? ['id' => $this->id] : [];
+        $result = $controller->{$route['method']}(...$params);
+    
+        if ($route['redirect'] ?? false) {
+            $prevUrl = $server['HTTP_REFERER'] ?? '/';
+            header("Location: {$prevUrl}");
+            return '';
+        }
+        return $result ?? '';
+    }
+
+    private function handleDefault(): string {
+        return (new HomeController())->get();
+    }
+
     public function route(string $url): string {
         $path = parse_url($url, PHP_URL_PATH);
         $pieces = explode("/", $path);
         $resource = $pieces[1];
+        $this->id = (isset($pieces[2])) ? intval($pieces[2]) : 0;
         $method = $_SERVER['REQUEST_METHOD'];
+
+        $routes = $this->getRoutes();
+        if (!isset($routes[$resource])) {
+            return $this->handleDefault();
+        }
+    
+        $route = $routes[$resource];
+    
+        // Обработка методов для ресурса (например, order)
+        if (isset($route[$method])) {
+            $route = $route[$method];
+        }
+    
+        return $this->executeRoute($route, $pieces);
+
+    /* Старый код с высокой сложностью из-за switch
         switch ($resource) {
             case "about":
                 $about = new AboutController();
@@ -42,6 +100,6 @@ class Router {
             default:
                 $home = new HomeController();
                 return $home->get();
-        }
+        }*/
     }
 }
